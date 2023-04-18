8eb5f0a22a1949721462777e7f772e3ed3c1ad74
yunfeilu-dev <93316392+yunfeilu-dev@users.noreply.github.com>
2023-04-18 09:12:07 +0800

    fix bash script

--- connect/create-alias.sh
+++ connect/create-alias.sh
@@ -9,10 +9,10 @@ alias_exists=$(aws lambda get-alias --function-name $FUNCTION_NAME --name $ALIAS
 
 if [ -z "$alias_exists" ]
 then
-  echo "Creating alias $ALIAS_NAME for function $FUNCTION_NAME"
+  echo "Creating alias \"$ALIAS_NAME\" for function \"$FUNCTION_NAME\"
   aws lambda create-alias --function-name $FUNCTION_NAME --name $ALIAS_NAME --function-version $target_version
 else
   echo "Alias $ALIAS_NAME already exists"
   current_version=$(aws lambda get-alias --function-name $FUNCTION_NAME --name $ALIAS_NAME | jq -r '.Version')
-  aws deploy create-deployment --application-name deployment-app --deployment-group-name lambda_deployment_group --revision '{"revisionType":"AppSpecContent","appSpecContent":{"content":"{\"version\":0,\"Resources\":[{\"game-demo-connect\":{\"Type\":\"AWS::Lambda::Function\",\"Properties\":{\"Name\":\"$FUNCTION_NAME\",\"Alias\":\"$ALIAS_NAME\",\"CurrentVersion\":\"$current_version\",\"TargetVersion\":\"$target_version\"}}}]}"}}'
+  aws deploy create-deployment --application-name deployment-app --deployment-group-name lambda_deployment_group --revision "{\"revisionType\":\"AppSpecContent\",\"appSpecContent\":{\"content\":\"{\\\"version\\\":0,\\\"Resources\\\":[{\\\"game-demo-connect\\\":{\\\"Type\\\":\\\"AWS::Lambda::Function\\\",\\\"Properties\\\":{\\\"Name\\\":\\\"$FUNCTION_NAME\\\",\\\"Alias\\\":\\\"$ALIAS_NAME\\\",\\\"CurrentVersion\\\":\\\"$current_version\\\",\\\"TargetVersion\\\":\\\"$target_version\\\"}}}]}\"}}"
 fi
