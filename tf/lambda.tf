a7cd4097cbcca2304c16c80a5c3522e9bc7ebdca
Yagr Xu <yagrxu@gmail.com>
2023-04-19 10:23:55 +0800

    update tf for alias in apigw and lambda

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -1,15 +1,12 @@
 resource "aws_lambda_function" "lambda_connect" {
-  filename         = "connect.zip"
+  filename         = "dummy.zip"
   function_name    = "game-demo-connect"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "index.handler"
   timeout          = 10
-  source_code_hash = filebase64sha256("connect.zip")
+  source_code_hash = filebase64sha256("dummy.zip")
   runtime          = "nodejs16.x"
-  # vpc_config {
-  #   subnet_ids = module.vpc.private_subnets
-  #   security_group_ids = [module.vpc.default_security_group_id]
-  # }
+  publish          = true
   tracing_config {
     mode = "Active"
   }
@@ -25,18 +22,18 @@ resource "aws_lambda_function" "lambda_connect" {
   }
 }
 
+
+
 resource "aws_lambda_function" "lambda_disconnect" {
-  filename         = "disconnect.zip"
+  filename         = "dummy.zip"
   function_name    = "game-demo-disconnect"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "index.handler"
   timeout          = 10
-  source_code_hash = filebase64sha256("disconnect.zip")
+  source_code_hash = filebase64sha256("dummy.zip")
   runtime          = "nodejs16.x"
-  # vpc_config {
-  #   subnet_ids = module.vpc.private_subnets
-  #   security_group_ids = [module.vpc.default_security_group_id]
-  # }
+  publish          = true
+
   tracing_config {
     mode = "Active"
   }
@@ -53,17 +50,40 @@ resource "aws_lambda_function" "lambda_disconnect" {
 }
 
 resource "aws_lambda_function" "lambda_default" {
-  filename         = "default.zip"
+  filename         = "dummy.zip"
   function_name    = "game-demo-default"
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "index.handler"
   timeout          = 10
-  source_code_hash = filebase64sha256("default.zip")
+  source_code_hash = filebase64sha256("dummy.zip")
   runtime          = "nodejs16.x"
-  # vpc_config {
-  #   subnet_ids = module.vpc.private_subnets
-  #   security_group_ids = [module.vpc.default_security_group_id]
-  # }
+  publish          = true
+
+  tracing_config {
+    mode = "Active"
+  }
+  environment {
+    variables = {
+      DELAYED_QUEUE_URL       = aws_ssm_parameter.delayed_queue_url.value
+      FIFO_QUEUE_URL          = aws_ssm_parameter.fifo_queue_url.value
+      FIFO_QUEUE_GROUP_ID     = aws_ssm_parameter.fifo_queue_group_id.value
+      PLAYER_TABLE_NAME       = aws_ssm_parameter.player_table_name.value
+      GAME_SESSION_TABLE_NAME = aws_ssm_parameter.game_session_table_name.value
+      DEFAULT_REGION          = aws_ssm_parameter.default_region.value
+    }
+  }
+}
+
+resource "aws_lambda_function" "lambda_logic" {
+  filename         = "dummy.zip"
+  function_name    = "game-demo-logic"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "index.handler"
+  timeout          = 10
+  source_code_hash = filebase64sha256("dummy.zip")
+  runtime          = "nodejs16.x"
+  publish          = true
+
   tracing_config {
     mode = "Active"
   }
@@ -80,29 +100,38 @@ resource "aws_lambda_function" "lambda_default" {
 }
 
 
-resource "aws_lambda_function" "lambda_logic" {
-  filename         = "logic.zip"
-  function_name    = "game-demo-logic"
-  role             = aws_iam_role.iam_for_lambda.arn
-  handler          = "index.handler"
-  timeout          = 10
-  source_code_hash = filebase64sha256("logic.zip")
-  runtime          = "nodejs16.x"
-  # vpc_config {
-  #   subnet_ids = module.vpc.private_subnets
-  #   security_group_ids = [module.vpc.default_security_group_id]
-  # }
-  tracing_config {
-    mode = "Active"
+resource "aws_lambda_alias" "game_demo_connect_alias_arn" {
+  name             = "prd"
+  function_name    = aws_lambda_function.lambda_connect.function_name
+  function_version = aws_lambda_function.lambda_connect.version
+  lifecycle {
+    ignore_changes = all
   }
-  environment {
-    variables = {
-      DELAYED_QUEUE_URL       = aws_ssm_parameter.delayed_queue_url.value
-      FIFO_QUEUE_URL          = aws_ssm_parameter.fifo_queue_url.value
-      FIFO_QUEUE_GROUP_ID     = aws_ssm_parameter.fifo_queue_group_id.value
-      PLAYER_TABLE_NAME       = aws_ssm_parameter.player_table_name.value
-      GAME_SESSION_TABLE_NAME = aws_ssm_parameter.game_session_table_name.value
-      DEFAULT_REGION          = aws_ssm_parameter.default_region.value
-    }
+}
+
+resource "aws_lambda_alias" "game_demo_disconnect_alias_arn" {
+  name             = "prd"
+  function_name    = aws_lambda_function.lambda_disconnect.function_name
+  function_version = aws_lambda_function.lambda_disconnect.version
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "aws_lambda_alias" "game_demo_default_alias_arn" {
+  name             = "prd"
+  function_name    = aws_lambda_function.lambda_default.function_name
+  function_version = aws_lambda_function.lambda_default.version
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "aws_lambda_alias" "game_demo_logic_alias_arn" {
+  name             = "prd"
+  function_name    = aws_lambda_function.lambda_logic.function_name
+  function_version = aws_lambda_function.lambda_logic.version
+  lifecycle {
+    ignore_changes = all
   }
 }
