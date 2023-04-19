d2de2e486ccf8aa06fbf4f89b7956dc683379df6
Yagr Xu <yagrxu@gmail.com>
2023-04-19 09:48:06 +0800

    add cicd

---
+++ cicd/codebuild.tf
@@ -0,0 +1,269 @@
+resource "aws_s3_bucket" "codebuild_s3" {
+  bucket_prefix = "devax-gaming-codebuild-"
+}
+
+
+data "aws_iam_policy_document" "codebuild_trusted_advisor" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "codebuild_iam_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["*"]
+    resources = ["*"]
+  }
+}
+
+
+resource "aws_iam_role" "codebuild_role" {
+  name               = "devax-gaming-codebuild-role"
+  assume_role_policy = data.aws_iam_policy_document.codebuild_trusted_advisor.json
+}
+
+
+resource "aws_iam_role_policy" "codebuild_role_policy" {
+  role   = aws_iam_role.codebuild_role.name
+  policy = data.aws_iam_policy_document.codebuild_iam_policy.json
+}
+
+
+resource "aws_codebuild_project" "build_project" {
+  name          = "build_project"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  cache {
+    type     = "S3"
+    location = aws_s3_bucket.codebuild_s3.bucket
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:2.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      name  = "TERRAFORM_VERSION"
+      value = "1.4.4"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "log-group"
+      stream_name = "log-stream"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+}
+
+resource "aws_codebuild_project" "connect_project" {
+  name          = "connect_project"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  cache {
+    type     = "S3"
+    location = aws_s3_bucket.codebuild_s3.bucket
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:2.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      name  = "TERRAFORM_VERSION"
+      value = "1.4.4"
+    }
+
+    environment_variable {
+      name  = "FUNCTION_NAME"
+      value = "game-demo-connect"
+    }
+    
+    environment_variable {
+      name  = "ALIAS_NAME"
+      value = "prd"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "log-group"
+      stream_name = "log-stream"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+}
+
+resource "aws_codebuild_project" "default_project" {
+  name          = "default_project"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  cache {
+    type     = "S3"
+    location = aws_s3_bucket.codebuild_s3.bucket
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:2.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      name  = "TERRAFORM_VERSION"
+      value = "1.4.4"
+    }
+
+    environment_variable {
+      name  = "FUNCTION_NAME"
+      value = "game-demo-default"
+    }
+
+    environment_variable {
+      name  = "ALIAS_NAME"
+      value = "prd"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "log-group"
+      stream_name = "log-stream"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+}
+
+resource "aws_codebuild_project" "disconnect_project" {
+  name          = "disconnect_project"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  cache {
+    type     = "S3"
+    location = aws_s3_bucket.codebuild_s3.bucket
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:2.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      name  = "TERRAFORM_VERSION"
+      value = "1.4.4"
+    }
+
+    environment_variable {
+      name  = "FUNCTION_NAME"
+      value = "game-demo-disconnect"
+    }
+
+    environment_variable {
+      name  = "ALIAS_NAME"
+      value = "prd"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "log-group"
+      stream_name = "log-stream"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+}
+
+resource "aws_codebuild_project" "logic_project" {
+  name          = "logic_project"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  cache {
+    type     = "S3"
+    location = aws_s3_bucket.codebuild_s3.bucket
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:2.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+
+    environment_variable {
+      name  = "TERRAFORM_VERSION"
+      value = "1.4.4"
+    }
+
+    environment_variable {
+      name  = "FUNCTION_NAME"
+      value = "game-demo-logic"
+    }
+
+    environment_variable {
+      name  = "ALIAS_NAME"
+      value = "prd"
+    }
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "log-group"
+      stream_name = "log-stream"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+}
+
