ebd45c4b14b7b6ae333b768eb71f954e4869540d
Yagr Xu <yagrxu@gmail.com>
2023-04-19 16:36:20 +0800

    fiox

--- cicd/mongodb.tf
+++ cicd/mongodb.tf
@@ -1,10 +1,10 @@
-resource "aws_ssm_parameter" "delayed_queue_url" {
+resource "aws_ssm_parameter" "mongodb_private_key" {
   name      = "/devax-game/mongodb_private_key"
   type      = "String"
   value     = var.mongodb_private_key
   overwrite = true
 }
-resource "aws_ssm_parameter" "delayed_queue_url" {
+resource "aws_ssm_parameter" "mongodb_public_key" {
   name      = "/devax-game/mongodb_public_key"
   type      = "String"
   value     = var.mongodb_public_key
