33c27271403833bfe7301fcd7e191795199ddf0f
Yagr Xu <yagrxu@gmail.com>
2023-04-19 10:04:34 +0800

    add buildspec

---
+++ lambda/default/create-alias.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+npm install 
+
+zip -r default.zip .
+target_version=$(aws lambda update-function-code --function-name $FUNCTION_NAME	--zip-file fileb://default.zip --publish | jq -r '.Version')
+alias_exists=$(aws lambda get-alias --function-name $FUNCTION_NAME --name $ALIAS_NAME 2>/dev/null)
+
+
+if [ -z "$alias_exists" ]
+then
+  echo "Creating alias $ALIAS_NAME for function $FUNCTION_NAME"
+  aws lambda create-alias --function-name $FUNCTION_NAME --name $ALIAS_NAME --function-version $target_version
+else
+  echo "Alias $ALIAS_NAME already exists"
+  current_version=$(aws lambda get-alias --function-name $FUNCTION_NAME --name $ALIAS_NAME | jq -r '.FunctionVersion')
+  aws deploy create-deployment --application-name default-app --deployment-group-name default_deployment_group --revision "{\"revisionType\":\"AppSpecContent\",\"appSpecContent\":{\"content\":\"{\\\"version\\\":0,\\\"Resources\\\":[{\\\"game-demo-connect\\\":{\\\"Type\\\":\\\"AWS::Lambda::Function\\\",\\\"Properties\\\":{\\\"Name\\\":\\\"$FUNCTION_NAME\\\",\\\"Alias\\\":\\\"$ALIAS_NAME\\\",\\\"CurrentVersion\\\":$current_version,\\\"TargetVersion\\\":$target_version}}}]}\"}}"
+fi
