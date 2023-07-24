f87caf8e453b7279e51dc14e8b5b74c444277659
Yagr Xu <yagrxu@gmail.com>
2023-07-24 14:33:47 +0800

    update code

---
+++ deploy-infra.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sh ./prebuild.sh
+
+export TFSTATE_KEY=gaming/demo/cannon-mosquito-server
+export TFSTATE_BUCKET=$(aws s3 ls --output text | awk '{print $3}' | grep tfstate-)
+export TFSTATE_REGION=us-east-1
+export AWS_DEFAULT_REGION=us-east-1
+
+cd tf
+# in case backend changed, please use '-reconfigure' parameter
+terraform init -backend-config="bucket=${TFSTATE_BUCKET}" -backend-config="key=${TFSTATE_KEY}" -backend-config="region=${TFSTATE_REGION}" -reconfigure
+
+terraform apply --auto-approve
